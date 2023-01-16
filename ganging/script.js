bcd9cb0265282a19cc12ee4dd3024e5b808385f5
Andreas <33508962+AndyMannen1@users.noreply.github.com>
2023-01-16 08:19:18 +0100

    Add files via upload

---
+++ ganging/script.js
@@ -0,0 +1,6 @@
+tall1 = prompt("Skriv in tall 1")
+tall2 = prompt("Skriv in tall 2")
+resultat = 0
+resultat = tall1 * tall2
+console.log(resultat)
+document.getElementById("resultat").innerHTML = "resultatet er:  " + resultat;
