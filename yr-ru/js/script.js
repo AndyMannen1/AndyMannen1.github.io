5adb66a28eea1c3c6bf05061f3e262252018e24f
Andreas <33508962+AndyMannen1@users.noreply.github.com>
2023-02-05 15:26:07 +0100

    Add files via upload

--- yr-ru/js/script.js
+++ yr-ru/js/script.js
@@ -1,28 +1,32 @@
-// fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.10&lon=9.58")
-// .then(res => {
-     
-//     if (res.ok){
-//         console.log("SUCCESS")
-//     } else {
-//         console.log("FAILURE")
-//     }
-//     res.json()
-// })
-
-// .then(data => console.log(data))    
-// .catch(error => console.log("ERROR"))'
 function askWeather(){
-fetch("https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=59.715948&lon=10.867377")
-.then(res => {
-    if (!res.ok) {
-        throw new Error("FAILURE: response not okay");
-    }
-    return res.json();
-})
-.then(data => {
-    const weatherData = data;
-    console.log(weatherData);
-    document.getElementById("lufttrykk").innerHTML="lufttrykket er: " + weatherData.properties.timeseries[0].data.instant.details.air_pressure_at_sea_level
-})
-.catch(error => console.error("ERROR: " + error.message));
+    let city = prompt("Skriv inn en by (ikke enkelte byggninger som drømtorp, men skriv heller Ski.):");
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ade61660873363fee0bb92057291f0d`)
+        .then(res => {
+            if (!res.ok) {
+                throw new Error("FAILURE: response not okay");
+            }
+            return res.json();
+        })
+        .then(data => {
+            const lat = data.coord.lat;
+            const lon = data.coord.lon;
+            var today = new Date();
+            var time = today.getHours();
+            fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`)
+                .then(res => {
+                    if (!res.ok) {
+                        throw new Error("FAILURE: response not okay");
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    const weatherData = data;
+                    console.log(weatherData);
+                    for (let i = 0; i <= 23; i++) {
+                        document.getElementById("tabelldata").innerHTML += "<tr><td> kl. " + (time + i) + "</td><td>" + "lufttemperatur dag " + i + " er: " + weatherData.properties.timeseries[i].data.instant.details.air_temperature + "</td>" + '<td> <img src="images/' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '.png" alt="' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '"/></td> </tr>';
+                    }
+                })
+                .catch(error => console.error("ERROR: " + error.message));
+        })
+        .catch(error => console.error("ERROR: " + error.message));
 }
