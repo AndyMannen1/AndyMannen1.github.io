f16516303ee8ea118429404f4407e2018ddf0cdc
Andreas <33508962+AndyMannen1@users.noreply.github.com>
2023-02-06 00:02:28 +0100

    Update script.js

--- yr-ru/js/script.js
+++ yr-ru/js/script.js
@@ -1,5 +1,7 @@
 function askWeather(){
-    let city = document.getElementById("input").innerHTML
+    //byen til openweathermap api
+    let city = prompt("Skriv inn en by (ikke enkelte byggninger som drømtorp, men skriv heller Ski.):");
+    //finner by-koordinater fra openweathermap api
     fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=6ade61660873363fee0bb92057291f0d`)
         .then(res => {
             if (!res.ok) {
@@ -8,10 +10,13 @@ function askWeather(){
             return res.json();
         })
         .then(data => {
+            //koordinater til MET api
             const lat = data.coord.lat;
             const lon = data.coord.lon;
+            // klokkeslett
             var today = new Date();
             var time = today.getHours();
+            //henter værdata fra MET
             fetch(`https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=${lat}&lon=${lon}`)
                 .then(res => {
                     if (!res.ok) {
@@ -20,13 +25,21 @@ function askWeather(){
                     return res.json();
                 })
                 .then(data => {
+                    //usortert data.
                     const weatherData = data;
                     console.log(weatherData);
+                    //loop som lar programmet vise data framover i tid
                     for (let i = 0; i <= 23; i++) {
-                        document.getElementById("tabelldata").innerHTML += "<tr><td> kl. " + (time + i) + "</td><td>" + "lufttemperatur dag " + i + " er: " + weatherData.properties.timeseries[i].data.instant.details.air_temperature + "</td>" + '<td> <img src="images/' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '.png" alt="' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '"/></td> </tr>';
+                        if(time > 24) {
+                            time = 1
+                        }
+                        // ubeskrivelig blokk med kode
+                        document.getElementById("tabelldata").innerHTML += "<tr><td> kl. " + (time ++) + "</td><td>" + weatherData.properties.timeseries[i].data.instant.details.air_temperature +"°" + "</td>" + '<td> <img src="images/' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '.png" alt="' + weatherData.properties.timeseries[i].data.next_1_hours.summary.symbol_code + '"/></td> </tr>';
                     }
                 })
+                //sier ifra om noe går galt
                 .catch(error => console.error("ERROR: " + error.message));
         })
+        //sier ifra om noe går galt
         .catch(error => console.error("ERROR: " + error.message));
 }
