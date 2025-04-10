ec864a14d2ed47da5dcfef91bd50d104e68febf0
AndyMannen1 <andreas.rovde@gmail.com>
2025-04-10 22:22:44 +0200

    C# greier

---
+++ Kalkulatorer/Kalkulatorer/Program.cs
@@ -0,0 +1,17 @@
+﻿
+using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Calculator 2: The Unwanted Sequel");
+
+            SelectionClass Selection = new SelectionClass();
+            Selection.RunSelect();
+        }
+
+    }
+}
---
+++ Kalkulatorer/Kalkulatorer/Selection.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Threading.Tasks.Dataflow;
+using System.Collections.Generic;
+using System.Text;
+using static System.Console;
+using Kalkulatorer;
+
+namespace Calculator
+{
+    public class SelectionClass()
+    {
+        public void RunSelect()
+        {
+            Title = "Calculator 2:";
+            RunMenu();
+
+        }
+
+        private void RunMenu()
+        {
+            string prompt = "Select a calculator:";
+            string[] options = { "Basic Calculator", "Super Basic Electromagnetism Calculator", "Exit" };
+            SelectionOptions MenuOptions = new SelectionOptions(prompt, options);
+            int SelectedIndex = MenuOptions.Run();
+
+            switch (SelectedIndex)
+            {
+                case 0:
+                    BasicCalculator();
+                    break;
+                case 1:
+                    SuperBasicElectromagnetismCalculator();
+                    break;
+                case 2:
+                    Exit();
+                    break;
+            }
+        }
+
+
+        private void BasicCalculator() {
+            WriteLine("Basic Calculator selected");
+        }
+
+        private void SuperBasicElectromagnetismCalculator()
+        {
+            WriteLine("Super Basic Electromagnetism Calculator selected");
+        }
+
+        private void Exit()
+        {
+            WriteLine("Exiting...");
+            Environment.Exit(0);
+        }
+
+    }
+}
